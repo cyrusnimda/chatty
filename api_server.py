edf15694514f4d9fe82b7c9e6baec75c8d66cbaf
Cyrus <cyrusnimda@gmail.com>
2014-03-30 21:10:21 +0200

    check token function

--- api_server.py
+++ api_server.py
@@ -14,6 +14,10 @@ app = Flask("apitest")
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify( { 'error': 'Not found' } ), 404)
+    
+@app.errorhandler(403)
+def required(error, msg):
+    return make_response(jsonify( { 'error': str(msg) + 'Required' } ), 403)
 
 
 def user_have_enoght_karma(needed_karma, user_karma):
@@ -44,22 +48,83 @@ def create_user():
 
 @app.route('/v1.0/temporal_user', methods = ['POST'])
 def create_temporal_user():
-    if not 'telephone' in request.json:
-        return jsonify( { 'status': "NOK", 'msg': "Needed data: telephone" } ), 302
-    temporal_user = TemporalUser(request.json['telephone'])
+    temporal_user = TemporalUser(request)
     if temporal_user.validate():
+        temporal_user.generateSmsCode()
         temporal_user.save()
+        return jsonify( { 'status': "OK", 'msg': "temporalUser created seccessfully" } ), 201
     else:
         return jsonify( { 'status': "NOK", 'msg': "Bad Data" } ), 302
-    return jsonify( { 'status': "OK", 'msg': "temporalUser created seccessfully" } ), 201
+    
+@app.route('/v1.0/activate_acount', methods = ['POST'])
+def activate_acount():
+	temporal_user = TemporalUser(request)
+	if not 'smsCode' in request.json:
+		return jsonify( { 'status': "NOK", 'msg': "smsCode is required" } ), 302
+	else:
+		temporal_user.setSmsCode(request.json['smsCode'])
+		
+	if temporal_user.exists():
+		user = User(request)
+		user.generateSecretToken()
+		user.insert()
+		return jsonify( { 'status': "OK", 'msg': "user created seccessfully", 'id' : user.getId(), 'secret_token' : user.getSecretToken() } ), 201
+	else:
+		return jsonify( { 'status': "NOK", 'msg': "SMS code does not match" } ), 302
 
+@app.route('/v1.0/user/rooms', methods = ['GET'])
+def getUserRooms():
+	pass
+	
+	
+class RequiredField(Exception):
+    status_code = 400
 
-    #exists = mongo.db.temporal_users.find({ "telephoneNumber": request.json['telephone'], "smsCode": request.json['sms_code'] } ).count()
-    #if exists==0:
-    #    return jsonify( { 'status': "NOK", 'msg': "SMS code does not match" } ), 201
-    #else:
-    #    return jsonify( { 'status': "OK", 'msg': "user created seccessfully" } ), 201
+    def __init__(self, message, status_code=None, payload=None):
+        Exception.__init__(self)
+        self.message = message
+        if status_code is not None:
+            self.status_code = status_code
+        self.payload = payload
+
+    def to_dict(self):
+        rv = dict(self.payload or ())
+        rv['status'] = 'NOK'
+        rv['msg'] = self.message
+        return rv
+	
+@app.errorhandler(RequiredField)
+def handle_required_field(error):
+    response = jsonify(error.to_dict())
+    response.status_code = 410
+    return response
+	
+def check_required(request, field_required):
+	if not field_required in request.json:
+		raise RequiredField('Field ' + field_required + ' is required')
+		
+def check_token(request):
+	if not "user" in request.json:
+		raise RequiredField('Field user is required')
+	if not "token" in request.json:
+		raise RequiredField('Field token is required')
+	
+	user =User()
+	user.find(id=request.json['user'])
+	if user.getSecretToken() != request.json['token']:
+		raise RequiredField('Secret token does not match')
+	
+@app.route('/v1.0/user', methods = ['GET'])
+def getUser():
+	check_required(request, "user_id")
+	check_token(request)
+	user = User().find(id=request.json['user_id'])
+	if user == False:
+		abort(404)
+	return jsonify(user.showDataTo(request.json['user'])), 201
+
 if __name__ == '__main__':
     app.run(debug = True)
     
+
 
--- models.py
+++ models.py
@@ -2,40 +2,141 @@
 import datetime
 from random import randrange
 from pymongo import MongoClient
+import uuid
+from bson.objectid import ObjectId
 
 class BaseCRUD():
     def __init__(self):
         mongoClient = MongoClient()
         self.mongo = mongoClient.apitest
+        self.updated_fields = []
+        self.id = None
+        
+    def getId(self):
+        return self.id
 
     def save(self):
         print "save" 
 
 class BaseApi(BaseCRUD):
-    def __init__(self, user, token):
-        self.user = user
-        self.token = token
-
-
-class TemporalUser(BaseCRUD):
-    def __init__(self, telephoneNumber):
+    def __init__(self):
         BaseCRUD.__init__(self)
-        self.telephoneNumber = telephoneNumber
-        self.smsCode = self.generateSmsCode()
+        self.errors = []
+
+
+class Room(BaseApi):
+    def __init__(self, request):
+        BaseApi.__init__(self, request)
+        self.collection = "rooms"
+        self.name = None
+        self.createtd_at = None
+        self.bloqued_users = []
+        self.silenced_users = []
+        self.public = None
+        self.owner = None
+        self.admins = []
+        self.users = []
+        self.picture = None
+        self.karma = None
+        self.config = None
+
+
+class User(BaseApi):
+    def __init__(self):
+        BaseApi.__init__(self)
+        self.collection = "users"
         self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
+        self.name = None
+        self.birth_date = None
+        self.city = None
+        self.picture = None
+        self.igonored_users = []
+        self.friends = []
+        self.config = None
+        self.karma = 30
+        self.secret_token = None
+        
+        
+    def toJSON(self):
+        pass 
+
+    def insert(self):
+        initUser = {'secret_token': self.secret_token, 'created_at': self.created_at, 'karma': self.karma, 'updated_at': self.updated_at}
+        self.id = str(self.mongo[self.collection].insert(initUser))
+    
+    def generateSecretToken(self):
+        fieldName = "secret_token"
+        if not fieldName in self.updated_fields:
+            self.updated_fields.append(fieldName)
+        self.secret_token = uuid.uuid4().hex
+        return True
+    
+    def getSecretToken(self):
+        return self.secret_token
+
+    def fillData(self, cursor):
+        print cursor
+        self.karma = cursor["karma"]
+        self.secret_token = cursor['secret_token']
+        
+    def showDataTo(self, user):
+        data = {}
+        data["karma"] = self.karma
+        return data
+
+    def find(self, id=None):
+        if id!= None:
+            try:
+                find = { "_id": ObjectId(id) }
+            except:
+                return False
+            cursor = self.mongo[self.collection].find(find)
+            if cursor.count()!=1:
+                return False
+            else:
+                self.fillData(cursor[0])
+        return self
+
+
+class TemporalUser(BaseApi):
+    def __init__(self, request):
+        BaseApi.__init__(self, request)
+        self.telephoneNumber = None
+        self.smsCode = None
+        self.created_at = datetime.datetime.now()
+        self.checkRequestData()
+        self.collection = "temporal_users"
+        
+    def setSmsCode(self, smsCode):
+        self.smsCode = smsCode
+
+    def checkRequestData(self):
+        if not 'telephone' in self.request.json:
+            self.validateRequest = False
+            self.errrors.append("Telephone is requiered.")
+        else:
+            self.telephoneNumber = self.request.json['telephone']
+        return True
         
     def generateSmsCode(self):
         code = ""
         for i in range(4):
             rand = randrange(10)
             code = code +str(rand)
-        return  code
+        self.smsCode = code
+        return  True
         
-    def validate(self):
+    def checkUserAndToken(self):
         return True
         
     def toJSON(self):
         return {'telephoneNumber':self.telephoneNumber, 'smsCode': self.smsCode, 'created_at': self.created_at}
 
     def save(self):
-        self.mongo.temporal_user.insert(self.toJSON())
+        self.mongo[self.collection].insert(self.toJSON())
+        
+    def exists(self):
+        find = { "telephoneNumber": self.telephoneNumber, "smsCode": self.smsCode }
+        count = self.mongo[self.collection].find( find).count()
+        return count>0

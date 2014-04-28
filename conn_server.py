5e7475fa5163f7ed1ed6828fee9273e7d984d272
Josu <josu.ruiz@iron-belt.com>
2014-04-28 17:48:37 +0200

    connection server test

---
+++ conn_server.py
@@ -0,0 +1,56 @@
+from twisted.internet.protocol import Factory
+from twisted.protocols.basic import LineReceiver
+from twisted.internet import reactor
+
+PORT = 1234
+
+class Chat(LineReceiver):
+
+    def __init__(self, users):
+        self.users = users
+        self.name = None
+        self.state = "GETNAME"
+
+    def connectionMade(self):
+    	self.clients=+1
+    	message = "Welcome to connection manager... send me your user id"
+        self.sendLine(message)
+
+    def connectionLost(self, reason):
+        if self.name in self.users:
+            del self.users[self.name]
+
+    def lineReceived(self, line):
+        if self.state == "GETNAME":
+            self.handle_GETNAME(line)
+        else:
+            self.handle_CHAT(line)
+
+    def handle_GETNAME(self, name):
+        if name in self.users:
+            self.sendLine("Name taken, please choose another.")
+            return
+        self.name = name
+        self.users[name] = self
+        self.sendLine("User accepted, we have now %s clients" % (len(self.users)))
+        self.state = "CHAT"
+
+    def handle_CHAT(self, message):
+        message = "<%s> %s" % (self.name, message)
+        for name, protocol in self.users.iteritems():
+            if protocol != self:
+                protocol.sendLine(message)
+
+
+class ChatFactory(Factory):
+
+    def __init__(self):
+        self.users = {} # maps user names to Chat instances
+
+    def buildProtocol(self, addr):
+        return Chat(self.users)
+
+
+reactor.listenTCP(PORT, ChatFactory())
+print "Waiting for connections at -%s- port" %(PORT)
+reactor.run()

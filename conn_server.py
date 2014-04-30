bc765af2992f38bd602cdd84df78a95691e0bce9
Josu <josu.ruiz@iron-belt.com>
2014-04-30 17:43:09 +0200

    rabbitmq tests

--- conn_server.py
+++ conn_server.py
@@ -1,8 +1,27 @@
 from twisted.internet.protocol import Factory
 from twisted.protocols.basic import LineReceiver
 from twisted.internet import reactor
+import pika
+import sys
+from threading import Thread
+from time import sleep
 
 PORT = 1234
+
+
+class RabbitMQ():
+    def __init__(self):
+        connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+        self.channel = connection.channel()
+
+        self.channel.exchange_declare(exchange='direct_logs',type='direct')
+
+        result = self.channel.queue_declare(exclusive=True)
+        self.queue_name = result.method.queue
+
+        self.channel.queue_bind(exchange='direct_logs', queue=self.queue_name, routing_key='info')
+
+
 
 class Chat(LineReceiver):
 
@@ -10,6 +29,8 @@ class Chat(LineReceiver):
         self.users = users
         self.name = None
         self.state = "GETNAME"
+        self.rabbitMQ = RabbitMQ()
+
 
     def connectionMade(self):
     	self.clients=+1
@@ -34,6 +55,21 @@ class Chat(LineReceiver):
         self.users[name] = self
         self.sendLine("User accepted, we have now %s clients" % (len(self.users)))
         self.state = "CHAT"
+        
+        thread = Thread(target = self.startConsuming)
+        thread.start()
+
+    def startConsuming(self):
+        print ' [*] Waiting for logs. To exit press CTRL+C'
+        self.rabbitMQ.channel.basic_consume(self.callback, queue=self.rabbitMQ.queue_name)
+        self.rabbitMQ.channel.start_consuming()
+
+    def callback(self, ch, method, properties, body):
+        print self.users
+        print " [x] %r:%r" % (method.routing_key, body,)
+        for name, protocol in self.users.iteritems():
+            protocol.sendLine(body)
+
 
     def handle_CHAT(self, message):
         message = "<%s> %s" % (self.name, message)
@@ -49,7 +85,6 @@ class ChatFactory(Factory):
 
     def buildProtocol(self, addr):
         return Chat(self.users)
-
 
 reactor.listenTCP(PORT, ChatFactory())
 print "Waiting for connections at -%s- port" %(PORT)
